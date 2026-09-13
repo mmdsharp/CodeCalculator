a8db5fe54f471b9262d3c6509816280f7dbb6979
Mohammad <mohammad.sha.af1.645748@gmail.com>
2026-09-13 14:52:00 +0430

    feat: add file-property

---
+++ CodeCalculator/FileProperty.cs
@@ -0,0 +1,63 @@
+﻿namespace CodeCalculator
+{
+    internal class FileProperty
+    {
+        public FileProperty()
+        {
+
+        }
+
+        public FileProperty(string fileName)
+        {
+            FileName = fileName;
+        }
+
+        private void FillCounts()
+        {
+            string fileContent = File.ReadAllText(FileName);
+
+            AllChars = fileContent.Length;
+            Spaces = fileContent.Count(c => c == ' ');
+            BreakLines = fileContent.Count(c =>
+                c == '\n' || c == '\r');
+
+            LinesCount = fileContent
+                .Replace("\r\n", "\n")
+                .Replace("\r", "\n")
+                .Count(c => c == '\n') + 1;
+        }
+
+        private string _fileName;
+
+        public string FileName
+        {
+            get
+            {
+                return _fileName;
+            }
+            set
+            {
+                _fileName = value;
+                FillCounts();
+            }
+        }
+
+
+        #region count
+
+        public long AllChars { get; private set; }
+        public long Spaces { get; private set; }
+        public long BreakLines { get; private set; }
+        public long SpacesAndBreakLines
+        {
+            get => Spaces + BreakLines;
+        }
+        public long ExceptSpacesAndBreakLines
+        {
+            get => AllChars - SpacesAndBreakLines;
+        }
+        public long LinesCount { get; set; }
+
+        #endregion 
+    }
+}
