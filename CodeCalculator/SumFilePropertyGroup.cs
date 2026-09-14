c3f4366eea7d0ebecd07143ed5316cda6e926f00
Mohammad <mohammad.sha.af1.645748@gmail.com>
2026-09-14 13:43:44 +0430

    feat: add sum-file-property-group

---
+++ CodeCalculator/SumFilePropertyGroup.cs
@@ -0,0 +1,30 @@
+﻿namespace CodeCalculator
+{
+    internal class SumFilePropertyGroup
+    {
+        public string Key { get; set; }
+        public SumFileProperty SumFileProperty { get; set; }
+
+        public static List<SumFilePropertyGroup> GroupByExtension(SumFileProperty sumFileProperty)
+        {
+            var groups = sumFileProperty.Files.GroupBy(f => GetExtension(f.FileName)).ToList();
+
+            var result = new List<SumFilePropertyGroup>();
+            foreach (var group in groups)
+            {
+                var item = new SumFilePropertyGroup()
+                {
+                    Key = group.Key,
+                    SumFileProperty = new(group.ToList())
+                };
+            }
+
+            return result;
+        }
+
+        private static string GetExtension(string fileName)
+        {
+            return fileName.Split('.').ToList().Last();
+        }
+    }
+}
